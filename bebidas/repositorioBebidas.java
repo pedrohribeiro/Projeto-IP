23bb133275dbc0c1e28f296e2e83e9ab0ab998f1
Pedro Henrique Ribeiro Pereira <phrp@WINDOWS.CIN.UFPE.BR>
2018-11-06 16:58:50 -0300

    Novo commit

--- bebidas/repositorioBebidas.java
+++ bebidas/repositorioBebidas.java
@@ -1,5 +1,15 @@
 package bebidas;
 
 public interface repositorioBebidas {
+	
+	public void inserir (Bebidas valor);
+	
+	public void remover (Bebidas valor);
+	
+	public void alterar (Bebidas valor);
+	
+	public boolean procurar (String tipo, String sabor);
+	
+	public void promocao (String tipo, double preco);
 
 }
--- bebidas/repositorioBebidasLista.java
+++ bebidas/repositorioBebidasLista.java
@@ -1,5 +1,49 @@
 package bebidas;
 
-public class repositorioBebidasLista {
+public class repositorioBebidasLista implements repositorioBebidas {
+	
+	private Bebidas valor;
+	private repositorioBebidasLista proximo;
+	
+	public repositorioBebidasLista() {
+		this.valor = null;
+		this.proximo = null;
+	}
+
+	public void inserir(Bebidas valor) {
+		
+		if (this.valor == null) {
+			this.valor = valor;
+			this.proximo = new repositorioBebidasLista();
+			
+		} else {
+			this.proximo.inserir(valor);
+		}
+	}
+
+	public void remover(String sabor) {
+		if(this.valor.getSabor() == sabor) {
+			this.valor= this.proximo.valor;
+			this.proximo = this.proximo.proximo;
+		} else {
+			this.proximo.remover(sabor);
+		}
+
+	}
+
+	public void alterar(String tipo, String sabor, char tamanho, double custo, double preco) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean procurar(String tipo, String sabor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void promocao(String tipo, double preco) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
